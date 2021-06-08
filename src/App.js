734e17b7595491dd6306883947c55df28540ea6d
marco <tonyriebeling@gmai.com>
2021-06-08 02:09:37 -0500

    s

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
-import React from "react";
+import React, { Component } from "react";
 import {Header} from "./componentes/header";               
-import 'boxicons';
+import 'boxicons'
+;
 import { BrowserRouter as Router } from "react-router-dom";
 import {Paginas} from "./componentes/Paginas";
 import { Dataprovider } from "./context/Dataprovider";
 import { Carrito } from "./componentes/Carrito";
 import { sesion } from "./componentes/sesion";
+import { render } from "@testing-library/react";
 
 
-function App() {
+function App () {
   return (
     <Dataprovider>
     <div className="App">
@@ -23,5 +25,7 @@ function App() {
     </Dataprovider>
   );
 }
+
+
 
 export default App;
--- src/componentes/header/index.js
+++ src/componentes/header/index.js
@@ -1,5 +1,6 @@
 import React,{useContext} from 'react'
 import logo from "../../img/logo.png";
+import logo1 from "../../img/logo1.png";
 import { Link} from "react-router-dom";
 import { DataContext} from "../../context/Dataprovider";
 
@@ -27,7 +28,7 @@ const tooggleMenu = () =>{
                     <Link to="/productos">PRODUCTOS</Link>
                 </li>
                 <li>
-                    <Link to="/Nosotros">NOSTROS</Link>
+                    <Link to="/Nosotros">NOSOTROS</Link>
                 </li>
                 <li>
                     <Link to="/Politicas">POLÍTICAS</Link>
@@ -35,7 +36,12 @@ const tooggleMenu = () =>{
                 <li>
                     <Link to="/sesion">INICIO DE SESION</Link>
                 </li>
+                <img  src={logo1} className="logo1" alt="logo1" width="20" />
+                <div className="" onClick="">
+                <input type="name" name="buscar" className="buscar" placeholder="Buscar..." />
+            </div>
             </ul>
+
             <div className="cart" onClick={tooggleMenu}>
                 <box-icon name="cart"></box-icon>
                 <span className="item__total">{carrito.length} </span>
